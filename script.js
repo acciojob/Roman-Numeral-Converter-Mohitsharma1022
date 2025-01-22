abdd0bf3d7755c321383e7b0f2ecc59802d0eb06
Mohit Sharma <167015033+Mohitsharma1022@users.noreply.github.com>
2025-01-22 14:26:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	let standard = [
+        ["I", 1],
+        ["IV", 4],
+     ["V", 5],
+    ["IX", 9],
+    ["X", 10],
+    ["XL", 40],
+    ["L", 50],
+    ["XC", 90],
+    ["C", 100],
+    ["CD", 400],
+    ["D", 500],
+    ["CM", 900],
+    ["M", 1000]
+]
+function getclosevalue(num){
 
-  //your code here
+	for(let i=0; i<standard.length; i++){
+		let current = standard[i][1];
+		if(current===num){
+			return standard[i]
+		}
+		if(current > num ){
+			return standard[i-1];
+		}
+	}
+	return "M";
+	
+}
+let ans = "";
+function convertToRoman(num) {	
+	
+	while(num != 0){
+		let roman = getclosevalue(num);
+
+		ans = ans.concat(num[0]);
+		num = num - roman[1];
+	}
+	return ans;
+};
+
+   
+
+  
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running the above function and printing it to console by pressing the run button at the top.
+// To run it with input 36, uncomment the following line
 
 // console.log(convertToRoman(36));
 
